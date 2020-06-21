b01a2243f53fb93de0dc6b9778ad8a9dc7016af0
Shagun Bandi <shagunbandi@dev.azure.com>
2020-06-22 01:05:53 +0530

    url

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import {
   Route,
 } from 'react-router-dom';
 import { SubNavBar } from './components/layout/NavbarComponents/SubNavBar';
-import {SideBar} from './components/layout/SidebarComponent'
+import { SideBar } from './components/layout/SidebarComponent'
 import Navbar from './components/layout/Navbar';
 import store from './store';
 import Footer from './components/layout/Footer';
-import { SkillsAndAchievment} from './components/SkillsAndAchievement';
-import {WorkExperience} from './components/WorkExperience';
-import { PersonalProject} from './components/PersonalProject';
+import { SkillsAndAchievment } from './components/SkillsAndAchievement';
+import { WorkExperience } from './components/WorkExperience';
+import { PersonalProject } from './components/PersonalProject';
 
 import './stylesheet/App.css';
 import './stylesheet/github1.css';
 import './stylesheet/github2.css';
 import { Summary } from './components/Summary';
-import {Photography} from './components/Photography';
+import { Photography } from './components/Photography';
 
 
 class App extends Component {
@@ -26,36 +26,36 @@ class App extends Component {
     return (
       <Provider store={store}>
         <Router>
-            <Navbar />
-                <div className="container-xl clearfix px-3 mt-4">
-                  <SideBar />
-                  <div className="col-lg-9 col-md-8 col-12 float-md-left pl-md-2" style={{ padding: "0px" }}>
-                    <SubNavBar />
-                    <div className="position-relative">
-                      <Switch>
-                        <Route path="/skills">
-                          <SkillsAndAchievment />
-                        </Route>
-                        <Route path="/workex">
-                          <WorkExperience />
-                        </Route>
-                        <Route path="/projects">
-                          <PersonalProject />
-                        </Route>
-                        <Route path="/overview">
-                          <Summary />
-                        </Route>
-                        <Route path="/photography">
-                          <Photography />
-                        </Route>
-                        <Route path="/">
-                          <Summary />
-                        </Route>
-                      </Switch>
-                    </div>
-                  </div>
-                </div>
-            <Footer />
+          <Navbar />
+          <div className="container-xl clearfix px-3 mt-4">
+            <SideBar />
+            <div className="col-lg-9 col-md-8 col-12 float-md-left pl-md-2" style={{ padding: "0px" }}>
+              <SubNavBar />
+              <div className="position-relative">
+                <Switch>
+                  <Route path="/skills">
+                    <SkillsAndAchievment />
+                  </Route>
+                  <Route path="/workex">
+                    <WorkExperience />
+                  </Route>
+                  <Route path="/projects">
+                    <PersonalProject />
+                  </Route>
+                  <Route path="/overview">
+                    <Summary />
+                  </Route>
+                  <Route path="/photography">
+                    <Photography />
+                  </Route>
+                  <Route path="/">
+                    <Summary />
+                  </Route>
+                </Switch>
+              </div>
+            </div>
+          </div>
+          <Footer />
         </Router>
       </Provider>
     );
